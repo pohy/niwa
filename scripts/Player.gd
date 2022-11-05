01279fe9c1b26815897c43c8aed22aba6be6effb
pohy <0ronon0@gmail.com>
2022-11-05 10:28:21 +0100

    Add basic physical player movement

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
@@ -11,19 +11,15 @@ onready var timer := $Timer as Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	timer.start()
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO
+	var velocity = Vector2.DOWN * 9.81
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -speed
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = speed
 		
-	position += velocity * delta
-
-
-func _on_Timer_timeout():
-	print_debug("waddup %s, %s" % [1, 2])
+	if velocity.length() > 0:
+		move_and_slide(velocity * speed)
