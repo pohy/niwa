8e6197b785016312fd354024e3c8c8af8bcbec9a
pohy <0ronon0@gmail.com>
2022-11-05 14:07:01 +0100

    Make plants stick to the ground

--- scripts/Flower.gd
+++ scripts/Flower.gd
@@ -1,18 +1,21 @@
 class_name Flower
 extends Area2D
 
+onready var ray_cast := $RayCast2D as RayCast2D
+onready var sprite := $Sprite as Sprite
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var placed: bool = false
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	print_debug("Wtf")
-	pass # Replace with function body.
+	sprite.visible = false
+	var pos = ray_cast.get_collision_point()
+	print_debug(ray_cast.get_collider())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if !placed and ray_cast.get_collider() != null:
+		position = ray_cast.get_collision_point()
+		ray_cast.enabled = false
+		sprite.visible = true
+		placed = true
